649507f12f27fac45d4e8b2107f903245a3e5e8a
Meach Sokhai <dshhshh750@gmail.com>
2026-03-24 19:33:32 +0700

    update upload image service

--- services/uploadImage.service.js
+++ services/uploadImage.service.js
@@ -1,9 +1,12 @@
 const cloudinary = require("../config/cloudinary.config");
+const fs = require("fs-extra");
 
 async function uploadImage(filePath) {
     const result = await cloudinary.uploader.upload(filePath,{
         folder: "attendance_teacher_image"
     });
+
+    await fs.removeSync(filePath);
 
     return {
         url: result.secure_url,
